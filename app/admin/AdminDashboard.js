f865a03f7cacd099b9e4018958dd426ab15e1a71
Mel Macario <xmelmacario@gmail.com>
2024-08-06 19:51:09 +0800

    added progress bar

--- app/admin/AdminDashboard.js
+++ app/admin/AdminDashboard.js
@@ -241,39 +241,13 @@ export default function AdminDashboard() {
             >
               <CardHeader className="flex flex-row items-start bg-muted/50">
                 <div className="grid gap-0.5">
-                  <CardTitle className="group flex items-center gap-2 text-lg">
-                    Kunwari Bargraph
-                  </CardTitle>
+                  
                 </div>
-
               </CardHeader>
-              <CardContent className="p-6 text-sm">
+              <CardContent className="p-3 text-sm">
                 <BarGraphReport />
-
                 <Separator className="my-4" />
                 <ChartReport />
-                <Separator className="my-4" />
-                <div className="grid gap-3">
-                  <div className="font-semibold">Customer Information</div>
-                  <dl className="grid gap-3">
-                    <div className="flex items-center justify-between">
-                      <dt className="text-muted-foreground">Customer</dt>
-                      <dd>Liam Johnson</dd>
-                    </div>
-                    <div className="flex items-center justify-between">
-                      <dt className="text-muted-foreground">Email</dt>
-                      <dd>
-                        <a href="mailto:">liam@acme.com</a>
-                      </dd>
-                    </div>
-                    <div className="flex items-center justify-between">
-                      <dt className="text-muted-foreground">Phone</dt>
-                      <dd>
-                        <a href="tel:">+1 234 567 890</a>
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
               </CardContent>
             </Card>
           </div>
--- app/admin/ChartReport.js
+++ app/admin/ChartReport.js
@@ -1,5 +1,6 @@
 import { CardDescription } from '@/components/ui/card';
-import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import { ChartContainer, ChartTooltipContent } from '@/components/ui/chart';
+import { Progress } from '@/components/ui/progress';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Area, AreaChart, XAxis, YAxis, Tooltip } from 'recharts';
@@ -18,7 +19,8 @@ function formatShortDate(dateString) {
 function ChartReport() {
   const [data, setData] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(33);
 
   const getTotalAmountForCurrentMonth = async () => {
     setIsLoading(true);
@@ -33,12 +35,20 @@ function ChartReport() {
       setData(fetchedData);
       setTotalAmount(totalAmountThisMonth);
 
+      setProgress(88);
+
       console.log("ChartReport.js => getTotalAmountForCurrentMonth() res: ", res);
     } catch (error) {
       toast.error("Something went wrong");
       console.log("ChartReport.js => getTotalAmountForCurrentMonth() error: ", error);
     } finally {
-      setIsLoading(false);
+      setTimeout(() => {
+        setProgress(100);
+      }, 500)
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000)
+
     }
   };
 
@@ -48,75 +58,83 @@ function ChartReport() {
 
   return (
     <>
-      <div>
-        <CardDescription>{"This month's total sales"}</CardDescription>
-      </div>
-      <div>
-        <div className="flex items-baseline gap-1 text-4xl tabular-nums mb-5">
-          {totalAmount}
-          <span className="font-sans text-sm font-normal tracking-normal text-muted-foreground">
-            Php
-          </span>
-        </div>
-        <ChartContainer
-          config={{
-            time: {
-              label: "Time",
-              color: "hsl(var(--chart-2))",
-            },
-          }}
-        >
-          <AreaChart
-            width={730}
-            height={250}
-            data={data}
-            margin={{
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0,
-            }}
-          >
-            <XAxis dataKey="date" tickFormatter={formatShortDate} />
-            <YAxis />
-            <Tooltip
-              content={<ChartTooltipContent hideLabel />}
-              formatter={(value, name, props) => (
-                <div className="flex flex-col items-start text-xs text-muted-foreground">
-                  <span>{formatDates(props.payload.date)}</span>
-                  <div className="flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
-                    {value}
-                    <span className="font-normal text-muted-foreground">
-                      php
-                    </span>
-                  </div>
-                </div>
-              )}
-            />
-            <defs>
-              <linearGradient id="fillTotalAmount" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-time)"
-                  stopOpacity={0.8}
+      {isLoading ? <div className="flex flex-col items-center w-100">
+        <Progress value={progress} className="w-[80%] mb-3" />
+        <CardDescription>{"Getting data..."}</CardDescription>
+      </div> :
+        <>
+          <div>
+            <CardDescription>{"This month's total sales"}</CardDescription>
+          </div>
+          <div>
+            <div className="flex items-baseline gap-1 text-4xl tabular-nums mb-5">
+              {totalAmount}
+              <span className="font-sans text-sm font-normal tracking-normal text-muted-foreground">
+                Php
+              </span>
+            </div>
+            <ChartContainer
+              config={{
+                time: {
+                  label: "Time",
+                  color: "hsl(var(--chart-2))",
+                },
+              }}
+            >
+              <AreaChart
+                width={730}
+                height={250}
+                data={data}
+                margin={{
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                }}
+              >
+                <XAxis dataKey="date" tickFormatter={formatShortDate} />
+                <YAxis />
+                <Tooltip
+                  content={<ChartTooltipContent hideLabel />}
+                  formatter={(value, name, props) => (
+                    <div className="flex flex-col items-start text-xs text-muted-foreground">
+                      <span>{formatDates(props.payload.date)}</span>
+                      <div className="flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                        {value}
+                        <span className="font-normal text-muted-foreground">
+                          php
+                        </span>
+                      </div>
+                    </div>
+                  )}
                 />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-time)"
-                  stopOpacity={0.1}
+                <defs>
+                  <linearGradient id="fillTotalAmount" x1="0" y1="0" x2="0" y2="1">
+                    <stop
+                      offset="5%"
+                      stopColor="var(--color-time)"
+                      stopOpacity={0.8}
+                    />
+                    <stop
+                      offset="95%"
+                      stopColor="var(--color-time)"
+                      stopOpacity={0.1}
+                    />
+                  </linearGradient>
+                </defs>
+                <Area
+                  dataKey="totalAmount"
+                  type="monotone"
+                  fill="url(#fillTotalAmount)"
+                  fillOpacity={0.4}
+                  stroke="var(--color-time)"
                 />
-              </linearGradient>
-            </defs>
-            <Area
-              dataKey="totalAmount"
-              type="monotone"
-              fill="url(#fillTotalAmount)"
-              fillOpacity={0.4}
-              stroke="var(--color-time)"
-            />
-          </AreaChart>
-        </ChartContainer>
-      </div>
+              </AreaChart>
+            </ChartContainer>
+          </div>
+        </>
+      }
+
     </>
   );
 }
